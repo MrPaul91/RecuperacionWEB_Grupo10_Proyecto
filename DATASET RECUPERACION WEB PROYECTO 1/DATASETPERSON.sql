512fbd973adc7b51bef52f7965f8affa296841db
Bryan and Juan Pablo Villegas Gómez <jupvillegasgo@unal.edu.co>
2018-04-18 06:39:22 -0500

    Final Commit

---
+++ DATASET RECUPERACION WEB PROYECTO 1/DATASETPERSON.sql
@@ -0,0 +1,52 @@
+﻿CREATE TABLE person(
+   birthName              VARCHAR(35) NOT NULL PRIMARY KEY
+  ,birthDate                DATE  NOT NULL
+  ,jobName                  VARCHAR(25) NOT NULL
+  ,RANGO_SALARIAL_EN_SMMLV VARCHAR(9) NOT NULL
+  ,hasEmployment                   BIT  NOT NULL
+);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('ABRIL LEON ÓSCAR CAMILO','5/22/12','TECNICO','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('ACUÑA CARLOS ALBERTO','1/29/10','TECNICO','=>2 y <4',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('AGUILAR DAZA CARMEN ELENA','2/2/16','DIRECTIVO','=>4 y <6',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('ALVARADO GONZÁLEZ AMANDA','1/1/99','TECNICO','=>2 y <4',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('BERNAL WESSO PAULA MILENA ALEJANDRA','2/8/16','TECNICO','=>2 y <4',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('CALCETO ARIAS ALEXANDRA','11/22/07','ASISTENCIAL','=1 y <2',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('CAMARGO RODRIGUEZ LUZ HELENA','1/1/99','TECNICO','=>2 y <4',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('CARBONELL LONDOÑO MIGUEL CRIST','12/9/10','TECNICO','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('CARREÑO AREVALO ERIKA VIVIANA','7/5/16','ASISTENCIAL','=1 y <2',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('CASTILLO ORTIZ NORMA BRIYITH','2/23/16','TECNICO','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('CHAVEZ OVIEDO ALEXANDRA','2/1/16','TECNICO','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('COLORADO GUAYACAN DAYANA IBETH','1/25/16','TECNICO','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('CRISTANCHO TOLEDO DAVID ENRIQUE','1/19/15','TECNICO','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('CUELLAR RIVERA JENNY PAOLA','11/1/15','TECNICO','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('FLOREZ SABOGAL LIZ TATIANA','7/8/14','TECNICO','=>2 y <4',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('GONZALEZ BOHORQUEZ LIDA MAYERLY','6/3/09','PROFESIONAL/ADMINISTRATIV','=>2 y <4',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('GUEVARA JHON ALEXANDER','10/8/10','ASISTENCIAL','=1 y <2',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('GUTIERREZ RAMIREZ MARIA DEL CA','1/1/99','ASISTENCIAL','=1 y <2',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('JURADO PUENTES ALEX FERNANDO','6/12/15','TECNICO','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('MÉNDEZ PÉREZ ÓSCAR OLIVERIO','12/12/14','TECNICO','=>4 y <6',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('MONTEALEGRE BARRERO LIZETH YAMILE','7/1/15','TECNICO','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('MORALES BERNAL RICARDO','3/1/98','PROFESIONAL/ADMINISTRATIV','=>2 y <4',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('PENA CRISTANCHO ALEXANDER','1/24/14','DIRECTIVO','=>8 y <11',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('PINZÓN VÁSQUEZ ANYILY LUCY','6/13/16','TECNICO','=1 y <2',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('PUERTO PAEZ CLAUDIA MARCELA','2/21/01','TECNICO','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('RAMIREZ BERNAL PAOLA XIMENA','10/21/14','TECNICO','=>2 y <4',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('RAMIREZ MONTANA JAIME','7/10/80','TECNICO','=1 y <2',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('RAVELO MARTINEZ LAZARO','8/14/01','TECNICO','=1 y <2',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('RENDON OSSA NELLY SOFIA','1/1/99','TECNICO','=1 y <2',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('RIVEROS NIETO MARIA CECILIA','5/6/03','TECNICO','=1 y <2',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('RODRIGUEZ TORRES HENRY','4/1/98','ASISTENCIAL','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('ROMERO TORRES YECID HUMBERTO','1/1/09','TECNICO','=>4 y <6',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('ROZO CASTILLO ELIANNE BERNEY','1/4/96','PROFESIONAL/ADMINISTRATIV','=>2 y <4',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('RUSSI SUAREZ GILMA CLEMENCIA','9/1/16','DIRECTIVO','=>6 y <8',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('SALAMANCA MONTANO HERNEY','1/1/99','TECNICO','=>2 y <4',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('SANCHEZ RUEDA CLAUDIA JIMENA','12/3/14','TECNICO','=>2 y <4',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('SIERRA ARAQUE RUTH BIBIANA','3/1/98','TECNICO','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('SUAREZ REYES PABLO ANTONIO','3/1/15','DIRECTIVO','=>6 y <8',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('TORRES GARZON ANDRES MAURICIO','3/2/16','ASISTENCIAL','=1 y <2',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('TORRES ROMERO MARCO ANTONIO','11/20/06','TECNICO','=>4 y <6',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('TRIANA GONZALO','10/9/06','ASISTENCIAL','=1 y <2',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('ULLOA ECHEVERRY FLAVIO ENRIQUE','10/17/13','DIRECTIVO','=>8 y <11',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('VARELA NEIRA AMARIS','4/1/98','TECNICO','=>2 y <4',0);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('PANTOJA EDUARDO ALIRIO','10/13/16','PROFESIONAL','=>8 y <11',1);
+INSERT INTO person(birthName,birthDate,jobName,RANGO_SALARIAL_EN_SMMLV,	hasEmployment) VALUES ('GOMEZ ARMIN OSWALDO','10/19/16','PROFESIONAL','=>6 y <8',1);
